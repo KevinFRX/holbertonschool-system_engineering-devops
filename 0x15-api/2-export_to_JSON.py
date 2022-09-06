0aa343881cbd5e28e93b8a7171c81d83d1d3487b
KevinFRX <xerokf13@gmail.com>
2022-09-06 13:07:43 -0700

    1

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+Using what you did in the task #0, extend your Python script
+to export data in the CSV format
+"""
+
+import json
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    ID = sys.argv[1]
+    user = requests.get("https://jsonplaceholder.typicode.com/users/{}".
+                        format(ID)).json()
+    username = user.get('username')
+    todos = requests.get("https://jsonplaceholder.typicode.com/todos").json()
+    taskº   _list = []
+    task_dict = {}
+
+    for todo in todos:
+        task_dict["task"] = todo.get('title')
+        task_dict["completed"] = todo.get('completed')
+        task_dict["username"] = username
+        task_list.append(task_dict)
+
+    obj = {}
+    obj[ID] = task_list
+
+    filename = ID + ".json"
+
+    with open(filename, "w") as f:
+        json.dump(obj, f)
