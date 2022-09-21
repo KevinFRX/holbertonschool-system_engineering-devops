3b9e18926ba177e355796c19741cc1b6e833819c
KevinFRX <xerokf13@gmail.com>
2022-09-21 23:07:09 +0000

    1.1

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,1 +1,24 @@
+#!/usr/bin/python3
+"""
+function that queries the Reddit API and prints the titles
+of the first 10 hot posts listed for a given subreddit
+"""
 
+import requests
+
+
+def top_ten(subreddit):
+    """comment"""
+    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'
+    }
+
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+        for post in response.json()['data']['children']:
+            print(post['data']['title'])
+    else:
+        print(None)
